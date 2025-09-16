8e54995af4d012806daae3c1f18486264be42791
reaganovechka <reaganovechka@u.boisestate.edu>
2025-09-16 17:48:52 -0600

    File not found

--- FormatChecker.java
+++ FormatChecker.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class FormatChecker {
     public static void main(String[] args) {
+        String fileName;
+        Boolean fileStatus = true;
+        
 
         //Read in file
-        
-        Scanner fileScnr = new Scanner(new File())
+        for (int i = 0; i < args.length; i++) {
+            fileName = args[i];
+            //read file
+            try {
+                Scanner fileScnr = new Scanner (new File(fileName));
+            } catch (FileNotFoundException e) {
+                System.out.println("java.io.FileNotFoundException: noSuchFile (The system cannot find the file specified)");
+                fileStatus = false;
+                System.out.print(fileStatus.toString());
+            }
+
+            if (fileStatus == true) {
+                
+            }
+
+            
+        }
     }
 }
