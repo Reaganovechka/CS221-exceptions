0018c6820d2ab67d4aa40a2cc3a8a997e21d8592
Reagan Ovechka <reaganovechka@u.boisestate.edu>
2025-09-18 17:57:41 -0600

    NumberFormatException

--- FormatChecker.java
+++ FormatChecker.java
@@ -9,26 +9,27 @@ public class FormatChecker extends Exception {
         int expectedRow;
         int expectedCol;
         double[][] grid;
+        String tempVal = null; // stores temp value before parsing to int/double
         
 
         //Read in file
         for (int i = 0; i < args.length; i++) {
-            fileName = args[i];
-            Boolean fileStatus = true;
+            fileName = args[i]; //get file name from command line
+            Boolean fileStatus = true; //Keeps track if file is valid or not
             Scanner fileScnr = null;
             Scanner lineScnr = null;
+
             //read file
             try {
                 //read file
                 fileScnr = new Scanner(new File(fileName));
                 System.out.println(fileName);
-                //catch exception below
 
                 //Get rows/ cols
                 String rowsAndCols = fileScnr.nextLine().trim(); 
                 lineScnr = new Scanner(rowsAndCols);
-                expectedRow = lineScnr.nextInt();
-                expectedCol = lineScnr.nextInt();
+                expectedRow = Integer.parseInt(lineScnr.next());
+                expectedCol = Integer.parseInt(lineScnr.next());
                 if (lineScnr.hasNext()){
                     throw new Exception ("too many row/col args");
                 }
@@ -45,7 +46,7 @@ public class FormatChecker extends Exception {
                         throw new Exception("Not enough rows!");
                     }
                     for (int col = 0; col < grid[row].length; col++) {                    
-                        grid[row][col] = lineScnr.nextDouble();
+                        grid[row][col] = Double.parseDouble(lineScnr.next());
                     }
                     if (lineScnr.hasNext()) {
                         throw new Exception ("Too many numbers in row " + (row + 1));
@@ -62,15 +63,12 @@ public class FormatChecker extends Exception {
             }
 
             catch (FileNotFoundException e) {
-                System.out.println("java.io.FileNotFoundException: " + fileName +  "(The system cannot find the file specified)");
+                System.out.println(e.toString());
                 fileStatus = false;
             }                
-            catch (InputMismatchException e) {
-                    System.out.println(e.toString() + ": Row/Col not integer");
-                    fileStatus = false;
-            }
             catch (NumberFormatException e) {
                 System.out.println(e.toString());
+                fileStatus = false;
             }
             catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println(e.toString());
