112958decaac52b4aa437338a5ead07f9a148feb
Reagan Ovechka <reaganovechka@u.boisestate.edu>
2025-09-18 12:07:57 -0600

    some more work

--- FormatChecker.java
+++ FormatChecker.java
@@ -32,16 +32,22 @@ public class FormatChecker {
 
                 //read in the data
                 grid = new double[expectedRow][expectedCol];
-                for (int row = 0; row < grid.length; row++) {
+            
+                for (int row = 0; row < grid.length; row++) { //Change the grid.length to expected row to get rid of that trailing line?? Should always have the proper amout??
                     String line = fileScnr.nextLine().trim();
                     lineScnr = new Scanner(line);
                     for (int col = 0; col < grid[row].length; col++) {
+                        if (grid[row].length == 0) {
+                            break;
+                        }                        
                         grid[row][col] = lineScnr.nextDouble();
                     }
                 }
 
             } 
-            catch 
+            catch (Exception e) {
+                if (e == error)
+            }
             // catch (FileNotFoundException e) {
             //     System.out.println("java.io.FileNotFoundException: " + fileName +  "(The system cannot find the file specified)");
             //     fileStatus = false;
