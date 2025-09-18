7651e9fe5da8f757064584215f0b70bf8ddc3ffe
Reagan Ovechka <reaganovechka@u.boisestate.edu>
2025-09-18 10:59:56 -0600

    row/ col not int exception

--- FormatChecker.java
+++ FormatChecker.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,21 +7,37 @@ public class FormatChecker {
     public static void main(String[] args) {
         String fileName;
         Boolean fileStatus = true;
+        int expectedRow;
+        int expectedCol;
         
 
         //Read in file
         for (int i = 0; i < args.length; i++) {
             fileName = args[i];
+            Scanner fileScnr = null;
+            Scanner lineScnr = null;
             //read file
             try {
-                Scanner fileScnr = new Scanner (new File(fileName));
+                fileScnr = new Scanner(new File(fileName));
             } catch (FileNotFoundException e) {
                 System.out.println("java.io.FileNotFoundException: noSuchFile (The system cannot find the file specified)");
                 fileStatus = false;
             }
 
-            if (fileStatus == true) {
-
+            //If the file is valid, start checking the format
+            if (fileStatus) {
+                String rowsAndCols = fileScnr.nextLine().trim(); 
+                lineScnr = new Scanner(rowsAndCols);
+                try {
+                    expectedRow = lineScnr.nextInt();
+                    expectedCol = lineScnr.nextInt();
+                    System.out.print(expectedRow);
+                    System.out.print(expectedCol);
+                }
+                catch (InputMismatchException e) {
+                    System.out.println(e.toString() + ": Row/Col not integer");
+                    fileStatus = false;
+                }
             }
 
             
